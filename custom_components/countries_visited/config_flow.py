20e6df14880e481820a1dba3652c0db5057114c6
Dmitriy Zaitsev <zaitsevds@ekassir.com>
2026-01-14 15:17:48 +0300

    fix: config_flow compatibility

--- custom_components/countries_visited/config_flow.py
+++ custom_components/countries_visited/config_flow.py
@@ -5,9 +5,20 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import config_validation as cv
-import homeassistant.helpers.entity_registry as er
 
 from .const import CONF_MAP_COLOR, CONF_PERSON, CONF_VISITED_COLOR, DOMAIN
+
+
+def get_person_entities(hass):
+    """Get list of person entity IDs."""
+    try:
+        return [
+            entity_id
+            for entity_id, entity in hass.entity_registry.entities.items()
+            if entity.domain == "person"
+        ]
+    except Exception:
+        return []
 
 
 class CountriesVisitedConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -25,26 +36,26 @@ class CountriesVisitedConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors = {}
         
-        # Get list of person entities
-        persons = [
-            entity.entity_id
-            for entity in self.hass.entity_registry.entities.values()
-            if entity.domain == "person"
-        ]
+        persons = get_person_entities(self.hass)
 
         if user_input is not None:
             return self.async_create_entry(title="Countries Visited", data=user_input)
 
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_PERSON): vol.In(persons) if persons else str,
+        if persons:
+            schema = {
+                vol.Required(CONF_PERSON, default=persons[0]): vol.In(persons),
                 vol.Optional(CONF_MAP_COLOR, default="#e0e0e0"): str,
                 vol.Optional(CONF_VISITED_COLOR, default="#4CAF50"): str,
             }
-        )
+        else:
+            schema = {
+                vol.Required(CONF_PERSON): str,
+                vol.Optional(CONF_MAP_COLOR, default="#e0e0e0"): str,
+                vol.Optional(CONF_VISITED_COLOR, default="#4CAF50"): str,
+            }
 
         return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=errors
+            step_id="user", data_schema=vol.Schema(schema), errors=errors
         )
 
 
@@ -58,24 +69,26 @@ class CountriesVisitedOptionsFlow(config_entries.OptionsFlow):
         """Manage the options."""
         errors = {}
         
-        persons = [
-            entity.entity_id
-            for entity in self.hass.entity_registry.entities.values()
-            if entity.domain == "person"
-        ]
+        persons = get_person_entities(self.hass)
+        current_person = self.config_entry.data.get(CONF_PERSON, "")
 
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_PERSON, default=self.config_entry.data.get(CONF_PERSON)): 
-                    vol.In(persons) if persons else str,
+        if persons:
+            schema = {
+                vol.Required(CONF_PERSON, default=current_person or persons[0]): 
+                    vol.In(persons),
                 vol.Optional(CONF_MAP_COLOR, default=self.config_entry.data.get(CONF_MAP_COLOR, "#e0e0e0")): str,
                 vol.Optional(CONF_VISITED_COLOR, default=self.config_entry.data.get(CONF_VISITED_COLOR, "#4CAF50")): str,
             }
-        )
+        else:
+            schema = {
+                vol.Required(CONF_PERSON, default=current_person): str,
+                vol.Optional(CONF_MAP_COLOR, default=self.config_entry.data.get(CONF_MAP_COLOR, "#e0e0e0")): str,
+                vol.Optional(CONF_VISITED_COLOR, default=self.config_entry.data.get(CONF_VISITED_COLOR, "#4CAF50")): str,
+            }
 
         return self.async_show_form(
-            step_id="init", data_schema=data_schema, errors=errors
+            step_id="init", data_schema=vol.Schema(schema), errors=errors
         )
